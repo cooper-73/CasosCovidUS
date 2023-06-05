c4fea1f1a077a2c6e5c920d22cef0fcdefd15f7c
cooper73 <c.cruzcoro73@gmail.com>
2023-06-05 17:34:12 -0500

    Main Activity UI started

--- app/src/main/java/com/example/casoscovidus/MainActivity.kt
+++ app/src/main/java/com/example/casoscovidus/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.casoscovidus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.casoscovidus.ui.fragments.ListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragment = ListFragment.newInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.fl_container, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/example/casoscovidus/ui/fragments/ListFragment.kt
@@ -0,0 +1,23 @@
+package com.example.casoscovidus.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.casoscovidus.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ListFragment()
+    }
+}
