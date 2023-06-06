796d21f51de8c6f0b194d8bd54e83e4b4a6d5100
cooper73 <c.cruzcoro73@gmail.com>
2023-06-06 18:07:57 -0500

    Modified model to fetch hash as id

--- app/src/main/java/com/example/casoscovidus/data/models/Report.kt
+++ app/src/main/java/com/example/casoscovidus/data/models/Report.kt
@@ -1,6 +1,10 @@
 package com.example.casoscovidus.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Report(
+    @SerializedName("hash")
+    val id: String,
     val date: Long,
     val positive: Long,
     val positiveIncrease: Long,
