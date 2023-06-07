7215b9aa177ca9b0b0774ce8defede1e66f2f713
cooper73 <c.cruzcoro73@gmail.com>
2023-06-07 00:40:27 -0500

    Added favorites retrieval feature to repository and view model

--- app/src/main/java/com/example/casoscovidus/data/repository/ReportsRepository.kt
+++ app/src/main/java/com/example/casoscovidus/data/repository/ReportsRepository.kt
@@ -1,19 +1,39 @@
 package com.example.casoscovidus.data.repository
 
 import androidx.lifecycle.MutableLiveData
+import com.example.casoscovidus.MyApp
+import com.example.casoscovidus.data.local.database.AppDatabase
 import com.example.casoscovidus.data.models.Report
 import com.example.casoscovidus.data.remote.RetrofitClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class ReportsRepository {
-    var reports = MutableLiveData<List<Report>>()
+    private val database = AppDatabase.getDatabase(MyApp.getContext())
+    val reports = MutableLiveData<List<Report>>()
+    val favorites = MutableLiveData<List<Report>>()
 
-    suspend fun fetchReportsList() {
+    suspend fun fetchReports() {
         return withContext(Dispatchers.IO) {
             val reportsResponse = RetrofitClient.reportsService.getReports()
+            database.reportDao().insertReports(reportsResponse)
+        }
+    }
+
+    suspend fun getReports() {
+        return withContext(Dispatchers.IO) {
+            val reportList = database.reportDao().getReports()
             withContext(Dispatchers.Main) {
-                reports.value = reportsResponse
+                reports.value = reportList
+            }
+        }
+    }
+
+    suspend fun getFavorites() {
+        return withContext(Dispatchers.IO) {
+            val favoriteList = database.reportDao().getFavorites()
+            withContext(Dispatchers.Main) {
+                favorites.value = favoriteList
             }
         }
     }
--- app/src/main/java/com/example/casoscovidus/viewmodels/ReportsViewModel.kt
+++ app/src/main/java/com/example/casoscovidus/viewmodels/ReportsViewModel.kt
@@ -1,37 +1,37 @@
 package com.example.casoscovidus.viewmodels
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.casoscovidus.data.models.Report
 import com.example.casoscovidus.data.repository.ReportsRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class ReportsViewModel : ViewModel() {
     private val repository = ReportsRepository()
-    
-    val reports: LiveData<List<Report>> = repository.reports
 
-    private val _favorites = MutableLiveData<List<Report>>()
-    val favorites: LiveData<List<Report>> = _favorites
+    val reports: LiveData<List<Report>> = repository.reports
+    val favorites: LiveData<List<Report>> = repository.favorites
+
+    init {
+        fetchReports()
+    }
+
+    fun fetchReports() {
+        viewModelScope.launch {
+            repository.fetchReports()
+        }
+    }
 
     fun loadReports() {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                repository.fetchReportsList()
-            }
+            repository.getReports()
         }
     }
 
     fun loadFavorites() {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                repository.fetchReportsList()
-            }
-            _favorites.value = reports.value?.filter { report -> report.isFavorite } ?: listOf()
+            repository.getFavorites()
         }
     }
 }
